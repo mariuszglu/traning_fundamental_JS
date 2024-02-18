1454732331ffd59f942e9ebe1e753474895b704d
Mariusz <pinkiweb88@gmail.com>
2024-02-18 21:04:52 +0100

    index6.js using modern JS ES6 create metods accelerate and brake in class Car

--- index6.js
+++ index6.js
@@ -3,31 +3,36 @@ class Car {
 		this.nameCar = nameCar;
 		this.speed = speed;
 	}
+
+	accelerate() {
+		this.speed = this.speed + 10;
+		this.getInformations();
+	}
+
+	brake() {
+		this.speed = this.speed - 5;
+		this.getInformations();
+	}
 }
 
-Car.prototype.accelerate = function () {
-	this.speed = this.speed + 10;
-    this.getInformations()
+// Car.prototype.accelerate = function () {
+// 	this.speed = this.speed + 10;
+//     this.getInformations()
+// };
+
+// Car.prototype.brake = function () {
+// 	this.speed = this.speed - 5;
+// 	this.getInformations();
+// };
+
+Car.prototype.getInformations = function () {
+	console.log(`${this.nameCar} going at ${this.speed} km/h`);
 };
-
-Car.prototype.brake = function(){
-    this.speed = this.speed - 5
-    this.getInformations()
-}
-
-Car.prototype.getInformations = function(){
-console.log(`${this.nameCar} going at ${this.speed} km/h`)
-}
 
 const car1 = new Car("Mercedes", 120);
 const car2 = new Car("BMW", 80);
 
-car1.accelerate()
-car2.accelerate()
-car1.brake()
-car2.brake()
-
-
-
-
-
+car1.accelerate();
+car2.accelerate();
+car1.brake();
+car2.brake();
